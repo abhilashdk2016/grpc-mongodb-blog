5c9e7a60de0d0335dd9288490df84bc9ceec31bb
abhilashdk2016 <abhilashdk2014@outlook.com>
2021-05-15 19:52:01 +0530

    gRPC Mongodb in C#

---
+++ client/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blog;
+using Grpc.Core;
+
+namespace client
+{
+    class MainClass
+    {
+        public async static Task Main(string[] args)
+        {
+            Channel channel = new Channel("localhost", 50052, ChannelCredentials.Insecure);
+            await channel.ConnectAsync().ContinueWith(task =>
+            {
+                if (task.Status == System.Threading.Tasks.TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully");
+                }
+            });
+
+            var client = new BlogService.BlogServiceClient(channel);
+            var newBlog = CreateNewBlog(client);
+            //ReadBlog(client);
+            //UpdateBlog(client, newBlog);
+            //DeleteBlog(client, newBlog);
+            await ListBlog(client);
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+
+        private static Blog.Blog CreateNewBlog(BlogService.BlogServiceClient client)
+        {
+            var response = client.CreatBlog(new CreateBlogRequest()
+            {
+                Blog = new Blog.Blog()
+                {
+                    AuthorId = "Abhilash",
+                    Content = "First Blog",
+                    Title = "New First Blog"
+                }
+            });
+
+            Console.WriteLine("The blog " + response.Blog.Id + " was created");
+            return response.Blog;
+        }
+
+        private static void ReadBlog(BlogService.BlogServiceClient client)
+        {
+            try
+            {
+                var response = client.ReadBlog(new ReadBlogRequest()
+                {
+                    BlogId = "609f98080201f0caaa80e5"
+                });
+                Console.WriteLine(response.Blog.ToString());
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+            }
+        }
+
+        private static void UpdateBlog(BlogService.BlogServiceClient client, Blog.Blog blog)
+        {
+            try
+            {
+                blog.Title = "Updated Title";
+                var response = client.UpdateBlog(new UpdateBlogRequest()
+                {
+                    Blog = blog
+                });
+                Console.WriteLine(response.Blog.ToString());
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+            }
+        }
+
+        private static void DeleteBlog(BlogService.BlogServiceClient client, Blog.Blog blog)
+        {
+            try
+            {
+                var response = client.DeleteBlog(new DeleteBlogrequest() { BlogId = blog.Id });
+                Console.WriteLine("The blog id " + blog.Id + " is deleted");
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+            }
+        }
+
+        private static async Task ListBlog(BlogService.BlogServiceClient client)
+        {
+            try
+            {
+                var response = client.ListBlog(new ListBlogRequest() { });
+                while(await response.ResponseStream.MoveNext())
+                {
+                    Console.WriteLine(response.ResponseStream.Current.Blog.ToString());
+                }
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+            }
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Blog;
+using Grpc.Core;
+
+namespace server
+{
+    class MainClass
+    {
+        const int PORT = 50052;
+        public static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Services = { BlogService.BindService(new BlogServiceImpl()) },
+                    Ports = { new ServerPort("localhost", PORT, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listning on port 50052");
+                Console.ReadKey();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("The Server failed to start : " + ex.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
